c3f465380f7653701beeddf54ba26286c60a5691
tetzank <tetzank@users.sourceforge.net>
2020-07-10 12:38:53 +0200

    added debug information into generated IR code
    It is now possible to break on entry of the generated function, step through
    each instruction and read the values of the variables.

--- sumDebug.cpp
+++ sumDebug.cpp
@@ -18,49 +18,98 @@
 #include <llvm/Transforms/IPO.h>
 #include <llvm/Transforms/IPO/PassManagerBuilder.h>
 
+#include <llvm/IR/DIBuilder.h>
+
 
 llvm::ExitOnError ExitOnErr;
 
 
 llvm::Function *generateFunction(llvm::LLVMContext &Context, llvm::Module &M){
-	// emit LLVM IR
-	//TODO: add debug information
+	// setup debug information
+	llvm::DIBuilder dibuilder(M);
+	//FIXME: hardcoded
+	llvm::DIFile *difile = dibuilder.createFile("sumDebug.cpp", "/home/franky/programming/github/llvm-jit-example_git");
+	//TODO: change bool when optimization is enabled
+	llvm::DICompileUnit *dicu = dibuilder.createCompileUnit(llvm::dwarf::DW_LANG_C, difile, "COAT", false, "", 0);
+
+	// IR types
 	llvm::Type *int64_type = llvm::Type::getInt64Ty(Context);
 	llvm::Type *int64_ptr_type = llvm::Type::getInt64PtrTy(Context);
+	// debug types
+	llvm::DIType *di_int64 = dibuilder.createBasicType("int64_t", 64, llvm::dwarf::DW_ATE_signed);
+	llvm::DIType *di_ptr_int64 = dibuilder.createPointerType(di_int64, 64, llvm::dwarf::DW_ATE_signed);
 
 	// Signature of the generated function.
 	llvm::FunctionType *jit_func_type = llvm::FunctionType::get(int64_type, {int64_ptr_type, int64_type}, false);
+	// IR function definition
 	llvm::Function *jit_func = llvm::Function::Create(jit_func_type, llvm::Function::ExternalLinkage, "sumfunc", M);
 
+	// debug definition of function
+	llvm::DISubprogram *disubprogram = dibuilder.createFunction(
+		difile, "sumfunc", "sumfunc", difile, 45,
+		dibuilder.createSubroutineType(dibuilder.getOrCreateTypeArray({di_int64, di_ptr_int64, di_int64})),
+		45,
+		llvm::DINode::DIFlags::FlagPrototyped,
+		llvm::DISubprogram::SPFlagDefinition
+	);
+	jit_func->setSubprogram(disubprogram);
+
+	// emit LLVM IR
 	llvm::BasicBlock *check_bb = llvm::BasicBlock::Create(Context, "check", jit_func);
 	llvm::BasicBlock *loop_bb = llvm::BasicBlock::Create(Context, "loop", jit_func);
 	llvm::BasicBlock *exit_bb = llvm::BasicBlock::Create(Context, "exit", jit_func);
 	llvm::IRBuilder<> builder(check_bb);
+
+	// unset location for prologue
+	builder.SetCurrentDebugLocation(llvm::DebugLoc());
 	// function arguments
 	llvm::Function::arg_iterator args = jit_func->arg_begin();
 	llvm::Value *arr = args++;
-	arr->setName("arr");
+	//arr->setName("arr");
 	llvm::Value *count = args++;
-	count->setName("count");
+	//count->setName("count");
+	// stack allocations for arguments
+	llvm::Value *addr_arr = builder.CreateAlloca(int64_ptr_type, nullptr, "arr");
+	llvm::Value *addr_count = builder.CreateAlloca(int64_type, nullptr, "count");
+	// create debug descriptors for the variables
+	llvm::DILocalVariable *di_arr = dibuilder.createParameterVariable(disubprogram, "arr", 0, difile, 45, di_ptr_int64, true);
+	dibuilder.insertDeclare(addr_arr, di_arr, dibuilder.createExpression(), llvm::DebugLoc::get(45, 0, disubprogram), builder.GetInsertBlock());
+	llvm::DILocalVariable *di_count = dibuilder.createParameterVariable(disubprogram, "count", 1, difile, 45, di_int64, true);
+	dibuilder.insertDeclare(addr_count, di_count, dibuilder.createExpression(), llvm::DebugLoc::get(45, 0, disubprogram), builder.GetInsertBlock());
+	// store parameters into stack allocated variables
+	builder.CreateStore(arr, addr_arr);
+	builder.CreateStore(count, addr_count);
 
 	// declarations
 	llvm::Value *addr_sum = builder.CreateAlloca(int64_type, nullptr, "sum");
-	llvm::Value *addr_i = builder.CreateAlloca(int64_type, nullptr, "i");
+	llvm::DILocalVariable *di_sum = dibuilder.createAutoVariable(disubprogram, "sum", difile, __LINE__-1, di_int64);
+	dibuilder.insertDeclare(addr_sum, di_sum, dibuilder.createExpression(), llvm::DebugLoc::get(__LINE__-2, 0, disubprogram), builder.GetInsertBlock());
 
+	llvm::Value *addr_i = builder.CreateAlloca(int64_type, nullptr, "i");
+	llvm::DILocalVariable *di_i = dibuilder.createAutoVariable(disubprogram, "i", difile, __LINE__-1, di_int64);
+	dibuilder.insertDeclare(addr_i, di_i, dibuilder.createExpression(), llvm::DebugLoc::get(__LINE__-2, 0, disubprogram), builder.GetInsertBlock());
+
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	builder.CreateStore(llvm::ConstantInt::get(int64_type, 0), addr_sum);
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	builder.CreateStore(llvm::ConstantInt::get(int64_type, 0), addr_i);
 
 	// if(count == 0)
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	llvm::Value *nullcheck = builder.CreateICmpEQ(count, llvm::ConstantInt::get(int64_type, 0), "nullcheck");
 	//     goto exit
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	builder.CreateCondBr(nullcheck, exit_bb, loop_bb);
 
 	builder.SetInsertPoint(loop_bb);
 	// addr = &arr[i], i - index into array
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	llvm::Value *addr = builder.CreateGEP(arr, builder.CreateLoad(addr_i), "addr");
 	// val = *addr
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	llvm::Value *val = builder.CreateLoad(addr, "val");
 	// new sum = old sum + val
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	builder.CreateStore(
 		builder.CreateAdd(
 			builder.CreateLoad(addr_sum),
@@ -69,6 +118,7 @@ llvm::Function *generateFunction(llvm::LLVMContext &Context, llvm::Module &M){
 		addr_sum
 	);
 	// new index = old index + 1
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	builder.CreateStore(
 		builder.CreateAdd(
 			builder.CreateLoad(addr_i),
@@ -77,14 +127,21 @@ llvm::Function *generateFunction(llvm::LLVMContext &Context, llvm::Module &M){
 		addr_i
 	);
 	// if(new index == count)
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	llvm::Value *cond = builder.CreateICmpEQ(builder.CreateLoad(addr_i), count, "cond");
 	// goto exit else goto loop
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	builder.CreateCondBr(cond, exit_bb, loop_bb);
 
 	builder.SetInsertPoint(exit_bb);
 	// return value
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	llvm::Value *ret = builder.CreateLoad(addr_sum);
+	builder.SetCurrentDebugLocation(llvm::DebugLoc::get(__LINE__+1, 0, disubprogram));
 	builder.CreateRet(ret);
+
+	// finalize debug information
+	dibuilder.finalize();
 
 	return jit_func;
 }
@@ -128,7 +185,7 @@ int main(){
 	// verify
 	verifyFunction(jit_func);
 
-#if 1
+#if 0
 	int optLevel = 3;
 
 	//TODO: use TransformLayer instead
